072bb58c027f9b416f8f571b76e4f9f72be99c89
Satyam <satyam.bppimt@gmail.com>
2020-08-17 00:56:01 +0530

    Adding documentation and postmap collection for API call simulation

--- src/main/java/com/fileManagementApp/filehandlerservice/FileHandlerServiceApplication.java
+++ src/main/java/com/fileManagementApp/filehandlerservice/FileHandlerServiceApplication.java
@@ -3,7 +3,10 @@ package com.fileManagementApp.filehandlerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class FileHandlerServiceApplication {
 
 	public static void main(String[] args) {
